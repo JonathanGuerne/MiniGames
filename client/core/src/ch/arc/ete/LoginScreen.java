f020d6ca24bde3c32cacfe08f747da12e9a78dc3
jonathan.guerne <jonathan.guerne@he-arc.ch>
2017-07-10 12:01:33 +0200

    auto-discovering server in the login screen

--- client/core/src/ch/arc/ete/LoginScreen.java
+++ client/core/src/ch/arc/ete/LoginScreen.java
@@ -6,19 +6,26 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
 
 import packets.LoginConfirmPacket;
 import packets.LoginPacket;
@@ -32,9 +39,12 @@ public class LoginScreen implements Screen {
     Label serverLabel;
     Label pseudoLabel;
     Label errorLabel;
+    Label serversListLabel;
     TextButton btnValider;
     TextField serverAdress;
     TextField clientPseudo;
+    SelectBox<String> serversAdresses;
+
     Stage stage;
     SpriteBatch sb;
     OrthographicCamera cam;
@@ -43,6 +53,7 @@ public class LoginScreen implements Screen {
     static int tcp = 23900, udp = 23901;
 
     boolean connectionOk = false;
+    boolean serverDiscoveringFinish = false;
 
 
     @Override
@@ -68,6 +79,8 @@ public class LoginScreen implements Screen {
         Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 
 
+        serversListLabel = new Label("Liste des serveurs : ",skin);
+
         serverLabel = new Label("Adresse du server : ", skin);
 
         pseudoLabel = new Label("Pseudo : ", skin);
@@ -77,20 +90,50 @@ public class LoginScreen implements Screen {
 
         btnValider = new TextButton("Connexion", skin);
 
+        serversAdresses = new SelectBox<String>(skin);
+        serversAdresses.setDisabled(true);
+
+
+        System.out.println("Discovering Hosts...");
+
+        class DiscoverHostThread implements Runnable {
+
+            @Override
+            public void run() {
+
+                Array<String> listAddressesString = new Array<String>();
+                java.util.List<InetAddress> listAddresses = client.discoverHosts(udp, 3000);
+                for (InetAddress adr : listAddresses) {
+                    if (!listAddressesString.contains(adr.getHostAddress(), false)) {
+                        listAddressesString.add(adr.getHostAddress());
+                    }
+                }
+                if (listAddressesString.size == 0) {
+
+                } else {
+                    serversAdresses.setItems(listAddressesString);
+                    serversAdresses.setDisabled(false);
+                    serverDiscoveringFinish = true;
+                }
+
+            }
+        }
+
+        new Thread(new DiscoverHostThread()).start();
+
         btnValider.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                if(clientPseudo.getText().equals(""))
-                {
+                if (clientPseudo.getText().equals("")) {
                     errorLabel.setText("Veuillez choisir un pseudo");
                     return;
                 }
                 try {
-                    client.connect(5000,serverAdress.getText(),tcp,udp);
+                    client.connect(5000, serverAdress.getText(), tcp, udp);
                     LoginPacket lp = new LoginPacket();
                     client.sendTCP(lp);
 
-                    while (!connectionOk){
+                    while (!connectionOk) {
 //                        System.out.println("waiting for server response");
                     }
 
@@ -105,17 +148,28 @@ public class LoginScreen implements Screen {
         });
 
 
-        client.addListener(new Listener(){
+        serversAdresses.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if (serverDiscoveringFinish) {
+                    serverAdress.setText(serversAdresses.getSelected());
+                }
+            }
+        });
+
+
+        client.addListener(new Listener() {
             @Override
             public void received(Connection connection, Object o) {
-                if(o instanceof Packet){
-                    if(o instanceof LoginConfirmPacket){
+                if (o instanceof Packet) {
+                    if (o instanceof LoginConfirmPacket) {
                         LoginConfirmPacket lcp = (LoginConfirmPacket) o;
                         connectionOk = true;
                     }
                 }
             }
         });
+
 
         serverAdress = new TextField("127.0.0.1", skin);
         clientPseudo = new TextField("", skin);
@@ -126,6 +180,10 @@ public class LoginScreen implements Screen {
         tableDisplay.add(pseudoLabel);
         tableDisplay.add(clientPseudo).width(300);
         tableDisplay.row();
+        tableDisplay.row();
+        tableDisplay.add(serversListLabel);
+        tableDisplay.add(serversAdresses).width(300);
+        tableDisplay.row();
         tableDisplay.add(errorLabel).colspan(2);
         tableDisplay.row();
         tableDisplay.add(btnValider).colspan(2);
@@ -133,7 +191,7 @@ public class LoginScreen implements Screen {
 
         stage.addActor(tableDisplay);
 
-        ((OrthographicCamera)stage.getCamera()).zoom = Util.getRatio()/2;
+        ((OrthographicCamera) stage.getCamera()).zoom = Util.getRatio() / 2;
 
         Gdx.input.setInputProcessor(stage);
 
@@ -153,7 +211,7 @@ public class LoginScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        ((OrthographicCamera)stage.getCamera()).zoom = Util.getRatio()/2;
+        ((OrthographicCamera) stage.getCamera()).zoom = Util.getRatio() / 2;
     }
 
     @Override
