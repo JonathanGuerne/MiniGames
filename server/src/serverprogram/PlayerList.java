303932c5d9dcb0e644ca3e1270e6595a2f4cef2f
jonathan.guerne <jonathan.guerne@he-arc.ch>
2017-07-13 08:56:05 +0200

    server check the version of all clients packet he receive

--- server/src/serverprogram/PlayerList.java
+++ server/src/serverprogram/PlayerList.java
@@ -14,34 +14,52 @@ public class PlayerList {
         this.listPlayer = new ArrayList<>();
     }
 
-    public void add(Player p){
+    public void add(Player p) {
         this.listPlayer.add(p);
     }
 
     /**
      * return a player giving his index in the list
+     *
      * @param index index in the list
      * @return player
      */
-    public Player getPlayerAt(int index){
+    public Player getPlayerAt(int index) {
         return listPlayer.get(index);
     }
 
     /**
      * get a player knowing his id
+     *
      * @param id id of the player
      * @return player
      */
     public Player getPlayerById(int id) {
         Player player = null;
 
-        for(Player p:listPlayer){
-            if(p.getIdPlayer() == id){
+        for (Player p : listPlayer) {
+            if (p.getIdPlayer() == id) {
                 player = p;
             }
         }
 
         return player;
+
+
+    }
+
+    /**
+     * check if a player with a specific id is in the player list
+     * @param id player id
+     * @return true or false in function
+     */
+    public boolean exist(int id) {
+        for(Player p:listPlayer){
+            if(p.getIdPlayer() == id){
+                return true;
+            }
+        }
+        return false;
     }
 }
 
